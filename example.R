4ceea2a6a387d83f08841ade5b46ce739172f99e
Oscar Gonzalez <oscgonfer@gmail.com>
2022-09-05 12:57:32 +0200

    Improve example after feedback

--- example.R
+++ example.R
@@ -1,5 +1,15 @@
-# source("R/apidevice.R")
-load_all()
+
+# Run this only the first time for installing the library
+# library(devtools)
+# install_github("fablabbcn/smartcitizen-R-data")
+
+library(smartcitizenRdata)
+# Change below to have the path to to the library (not needed in RStudio in OSx)
+# load_all(path="/path/to/smartcitizen-R-data")
+# source("apidevice.R") #script and functions to be used
+
+# Change below to set your working directory (where data will go)
+setwd("/path/to/data/folder")
 
 # Input here device ID
 device_id <- 14675
@@ -19,3 +29,9 @@ d$get_device_data()
 
 # Tabular data
 data <- d$data
+
+# Change the xts format to dataframe (matrix)
+data <- data.frame(date= index(data), coredata(data))
+
+# Save the data to a txt file
+write.table(data, file=sprintf("%s.csv", device_id),  dec=".", sep ="\t", col.names=T, row.names=F, append=F)
